42a1ab51d2c88f520c5ea34503e77787bf84a932
Tobi Oloke <tobzdaman619@gmail.com>
2024-10-31 23:33:54 +0000

    New approach - just read the GameplayState whenever a new key event is added by briefly setting a lastkey variable

--- src/Logging/Logger.hs
+++ src/Logging/Logger.hs
@@ -5,22 +5,33 @@ module Logging.Logger where
 import Bluefin
 import Bluefin.Compound
 import Bluefin.Eff
+import Bluefin.IO (IOE)
+import Bluefin.Reader
 import Bluefin.State
 import Bluefin.Stream
 import Bluefin.Writer
-import Brick.Types (EventM)
+import qualified Brick.Keybindings as K
+import Brick.Types(BrickEvent(VtyEvent), EventM)
 import GameLogic (GameState)
+import Graphics.Vty.CrossPlatform as V
+import qualified Graphics.Vty.Input as V
 import UI.Gameplay
-import Bluefin.IO (IOE)
 import Unsafe.Coerce (unsafeCoerce)
 
 type TickNumber = Int
 
-data LogAction = MovedUp | MovedDown | MovedLeft | MovedRight 
+data LogAction = MovedUp | MovedDown | MovedLeft | MovedRight
   deriving (Show, Eq)
 
-convertToMovement :: EventM n GameState a -> LogAction
-convertToMovement _ = undefined
+convertToMovement ev@(VtyEvent (V.EvKey k mods)) = do
+  handleGameplayEvent' ev
+  disp <- case gameplayDispatcher [] of
+    Right disp -> return disp
+    Left _ -> undefined
+  _ <- K.handleKey disp k mods
+  return ()
+
+convertToMovement _ = return ()
 
 -- | Pairing of tick events to significant moves
 data GameEvent = GameEvent TickNumber LogAction
@@ -29,12 +40,9 @@ type EventHistory e = Writer EventList e
 
 type EventList = [GameEvent]
 
-data Event n s a es = Event (EventM n s a) es
-
 data Logger n a es = Logger
   { log :: Writer EventList es,
-    events :: Stream GameEvent es,
-    ticks :: State TickNumber es
+    gameState :: Reader GameplayState es
   }
 
 appendGameEvent :: EventList -> (TickNumber, LogAction) -> EventList
@@ -45,15 +53,3 @@ incTicks st = modify st (+ 1)
 
 addToLog :: (e :> es) => EventHistory e -> GameEvent -> Eff es ()
 addToLog hist ev = tell hist (pure ev)
-
-getEvents = runEff $ \ioe -> yieldToList $ \s -> do
-  (tick , action) <- undefined
-  let conv = convertToMovement action
-  yield s (GameEvent tick conv)
-
-eventMtoEvent :: EventM n s a -> Event n s a e
-eventMtoEvent = unsafeCoerce
-
-convertEvent :: Event n s a e -> LogAction
-convertEvent ev = case runEvent ev of
-  undefined -> undefined
