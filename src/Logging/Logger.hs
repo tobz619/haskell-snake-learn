84c34b8331d77925fdca4732ab62e67ea7b3daf7
Tobi Oloke <tobzdaman619@gmail.com>
2024-12-06 10:54:42 +0000

    Removing redundant imports

--- src/Logging/Logger.hs
+++ src/Logging/Logger.hs
@@ -3,28 +3,21 @@
 
 module Logging.Logger where
 
-import Bluefin
-import Bluefin.Compound (Handle (..), useImplIn, useImpl)
-import Bluefin.Eff (Eff, runPureEff, (:&), type (:>))
-import Bluefin.IO (IOE)
-import Bluefin.Reader (Reader, ask, runReader)
-import Bluefin.State
-import Bluefin.Stream
-import Bluefin.Writer (Writer, execWriter, runWriter, tell)
+import Bluefin.Compound (Handle (..), useImplIn)
+import Bluefin.Eff (Eff, (:&), type (:>))
+import Bluefin.Reader (Reader, runReader)
+import Bluefin.Writer (Writer, execWriter, tell)
 import Brick (BrickEvent (..))
 import qualified Brick.Keybindings as K
-import Brick.Types (EventM)
-import GameLogic (GameState)
 import Graphics.Vty.CrossPlatform as V ()
 import qualified Graphics.Vty.Input as V
-import Lens.Micro ((^.))
 import UI.Keybinds
 
 type TickNumber = Int
 
 -- | Pairing of tick events to significant moves
 data GameEvent = GameEvent TickNumber KeyEvent
-  deriving Show
+  deriving (Show)
 
 type EventList = [GameEvent]
 
@@ -50,13 +43,12 @@ getKeyEvent dispatcher altConfig (VtyEvent (V.EvKey k mods)) = do
 getKeyEvent _ _ _ = Nothing
 
 addToLog :: (e :> es) => Writer EventList e -> TickNumber -> Maybe KeyEvent -> Eff es ()
-addToLog strm tick = maybe
-                      (pure ()) -- Do nothing if the key is not found
-                      (tell strm . pure . GameEvent tick) -- otherwise, write it to the logger
+addToLog strm tick =
+  maybe
+    (pure ()) -- Do nothing if the key is not found
+    (tell strm . pure . GameEvent tick) -- otherwise, write it to the logger
 
-runLogger :: (forall e1. Logger g e1 -> Eff (e1 :& es) r ) -> g -> Eff es [GameEvent]
+runLogger :: (forall e1. Logger g e1 -> Eff (e1 :& es) r) -> g -> Eff es [GameEvent]
 runLogger f gps = runReader gps $ \rea -> do
-                    execWriter $ \y -> do
-                      useImplIn f (Logger (mapHandle y) (mapHandle rea))
-
-
+  execWriter $ \y -> do
+    useImplIn f (Logger (mapHandle y) (mapHandle rea))
