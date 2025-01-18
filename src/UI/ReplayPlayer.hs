17140b0ef5cf9f2be9799588f29173ccde1531f8
Tobi Oloke <tobzdaman619@gmail.com>
2025-01-18 19:28:49 +0000

    Game can play itself!

--- src/UI/ReplayPlayer.hs
+++ src/UI/ReplayPlayer.hs
@@ -1,60 +1,82 @@
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE NumericUnderscores #-}
-module ReplayPlayer where
+{-# LANGUAGE RankNTypes #-}
 
-import Brick.Types (Widget)
-import Logging.Replay (ReplayState (..), stepReplayState, executeMove)
-import UI.Gameplay (MenuOptions, drawGS, theMap, Tick (Tick))
+module UI.ReplayPlayer where
+
+import Brick (App (..), BrickEvent (AppEvent, VtyEvent), EventM, customMain, get, modify, neverShowCursor, put)
 import Brick.BChan (newBChan, writeBChan)
-import Control.Concurrent (threadDelay, forkIO)
-import qualified Graphics.Vty.CrossPlatform as V
-import qualified Graphics.Vty.Config as V
+import Brick.Main (halt)
+import Brick.Types (Widget)
+import Control.Concurrent (MVar, forkIO, newMVar, putMVar, readMVar, takeMVar, threadDelay, swapMVar)
 import Control.Monad (forever, void, when)
-import Brick (customMain, App (..), neverShowCursor, BrickEvent (AppEvent), get, modify, EventM, put)
-import GameLogic (initWorld, defaultHeight, defaultWidth, GameState (Playing))
-import System.Random (StdGen)
+import Control.Monad.IO.Class (liftIO)
+import GameLogic (GameState (Playing), defaultHeight, defaultWidth, initWorld)
+import qualified Graphics.Vty as V
+import qualified Graphics.Vty.CrossPlatform as V
 import Logging.Logger (EventList, GameEvent (GameEvent))
+import Logging.Replay (ReplayState (..), executeMove, stepReplayState)
+import System.Random (StdGen, mkStdGen)
+import UI.Gameplay (MenuOptions, Tick (Tick), drawGS, theMap)
+import UI.Keybinds (KeyEvent (..))
 
-replay seed = do
+runReplayApp :: StdGen -> MVar EventList -> IO ()
+runReplayApp seed mEvList = do
   chan <- newBChan 10
   _ <- forkIO $ forever $ do
-      writeBChan chan Tick
-      threadDelay 100_000
+    writeBChan chan Tick
+    threadDelay 100_000
   let initialVty = V.mkVty V.defaultConfig
   buildVty <- initialVty
-  void $ customMain buildVty initialVty (Just chan) replayApp (initState seed)
+  void $ customMain buildVty initialVty (Just chan) (replayApp mEvList) (initState seed)
 
-replayApp :: App ReplayState Tick MenuOptions
-replayApp =
-    App
-        { appDraw = drawUI
-        , appChooseCursor = neverShowCursor
-        , appHandleEvent = replayEventHandler
-        , appStartEvent = return ()
-        , appAttrMap = const theMap
-        }
+replayApp :: MVar EventList -> App ReplayState Tick MenuOptions
+replayApp evs =
+  App
+    { appDraw = drawUI,
+      appChooseCursor = neverShowCursor,
+      appHandleEvent = flip replayEventHandler evs,
+      appStartEvent = replayEventHandler (AppEvent Tick) evs,
+      appAttrMap = const theMap
+    }
 
-replayEventHandler :: BrickEvent n Tick -> EventM n (EventList, ReplayState) ()
-replayEventHandler (AppEvent Tick) = do
-  (evs, rps) <- get
-  modify $ \(e, r) -> (e, stepReplayState r)
-  when (canExecute evs rps) $ makeMove evs rps
-replayEventHandler _ = return ()
+replayEventHandler :: BrickEvent n Tick -> MVar EventList -> EventM n ReplayState ()
+replayEventHandler (VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl])) _ = halt
+replayEventHandler (AppEvent Tick) mVarEv = do
+  modify stepReplayState
+  rps <- get
+  evlist <- liftIO $ readMVar mVarEv
+  when (canExecute evlist rps) $ do
+    newEvs <- makeMove evlist rps
+    _ <- liftIO $ swapMVar mVarEv newEvs
+    return ()
 
-canExecute :: [GameEvent] -> ReplayState -> Bool
+replayEventHandler _ _ = return ()
+
+canExecute :: EventList -> ReplayState -> Bool
 canExecute [] _ = False
-canExecute (GameEvent t0 _: _) (ReplayState _ t1) = t0 == t1
+canExecute ((GameEvent t0 _):_) (ReplayState _ t1) = t0 == t1
 
-makeMove :: EventList -> ReplayState -> EventM n (EventList, ReplayState) ()
-makeMove [] _ = return ()
-makeMove (GameEvent _ ev: res) rps = 
-  put $ (res, rps {gameState = executeMove ev (gameState rps)})
-
+makeMove :: EventList -> ReplayState -> EventM n ReplayState EventList
+makeMove [] _ = return []
+makeMove (GameEvent _ ev : res) rps =
+  do
+    put $ rps {gameState = executeMove ev (gameState rps)}
+    return res
 
 initState :: StdGen -> ReplayState
-initState seed = ReplayState {tickNo=0, gameState=start}
-  where start = Playing $ initWorld defaultHeight defaultWidth seed
+initState seed = ReplayState {tickNo = 0, gameState = start}
+  where
+    start = Playing $ initWorld defaultHeight defaultWidth seed
 
 drawUI :: ReplayState -> [Widget MenuOptions]
 drawUI rps = drawGS gs
   where
     gs = gameState rps
+
+replayExample :: IO ()
+replayExample = do
+  evs <- newMVar events
+  runReplayApp (mkStdGen 4) evs
+  where
+    events = zipWith GameEvent (iterate (+ 3) 3) (cycle [MoveLeft, MoveDown, MoveRight, MoveUp])
