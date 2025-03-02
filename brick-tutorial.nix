c23449883775caa712b8c0eb59a86e15ed838529
Tobi Oloke <tobzdaman619@gmail.com>
2025-03-02 20:34:45 +0000

    Working server implementation, now just got to fix how event lists are sent across

--- brick-tutorial.nix
+++ brick-tutorial.nix
@@ -1,19 +1,19 @@
-{ mkDerivation, base, bluefin, brick, bytestring, containers, lib
-, linear, microlens, microlens-mtl, microlens-th, mtl
-, nonempty-containers, random, scotty, sqlite-simple, text, time
-, vector, vty, vty-crossplatform, websockets, word-wrap
+{ mkDerivation, base, bimap, binary, bluefin, brick, bytestring
+, containers, lib, linear, microlens, microlens-mtl, microlens-th
+, mtl, nonempty-containers, random, scotty, sqlite-simple, text
+, time, vector, vty, vty-crossplatform, websockets, word-wrap
 }:
 mkDerivation {
   pname = "brick-tutorial";
   version = "0.1.0.0";
-  src = /nix/store/grnxzdqwmanc9y04hbaswfk4gd58kha6-brick-tutorial;
+  src = /nix/store/a1wh0ghrrvb5vyzh6hyiy3c6qxi7cngc-brick-tutorial;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base bluefin brick bytestring containers linear microlens
-    microlens-mtl microlens-th mtl nonempty-containers random scotty
-    sqlite-simple text time vector vty vty-crossplatform websockets
-    word-wrap
+    base bimap binary bluefin brick bytestring containers linear
+    microlens microlens-mtl microlens-th mtl nonempty-containers random
+    scotty sqlite-simple text time vector vty vty-crossplatform
+    websockets word-wrap
   ];
   executableHaskellDepends = [ base brick vty vty-crossplatform ];
   testHaskellDepends = [ base ];
