6706665a2fe4563ba788813ad5982accf63d1fb8
Tobi Oloke <tobzdaman619@gmail.com>
2025-03-11 13:23:38 +0000

    Refactored to use raw TCP; WebSockets do indeed work but I'll take more control this way. Testing TLS through caddy

--- src/DB/Client.hs
+++ src/DB/Client.hs
@@ -1,29 +1,54 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TypeApplications #-}
 
 module DB.Client where
 
+
+import Control.Concurrent.Async
+import Control.Monad (replicateM_)
 import DB.Highscores (Name)
 import Data.Bimap (Bimap)
 import qualified Data.Bimap as BM
-import Data.Binary (encode)
+import Data.Bits (FiniteBits (finiteBitSize))
 import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Char8 as B8 
 import qualified Data.ByteString.Lazy as B
+-- import Data.Int (Int64)
+import Data.List (scanl')
 import Data.Maybe (fromMaybe)
 import qualified Data.Text as Text
+import qualified Data.Text.Encoding as Text
+import qualified Data.Text.Lazy as TextL
+import Data.Word (Word32, Word8)
+import GHC.Conc
+import GHC.Generics (Generic)
+import GHC.IO (finally, mask)
 import GameLogic (ScoreType)
 import Logging.Logger (EventList, GameEvent (..), TickNumber (..))
-import qualified Network.WebSockets as WS
+import Network.Socket
+import Network.Socket.ByteString.Lazy ( sendAll )
+import Network.TLS
+import System.Random (mkStdGen)
 import UI.Gameplay (SeedSize)
 import UI.Keybinds (KeyEvent (..))
-import Data.List (scanl')
-import Control.Monad (replicateM_)
-import System.Random (mkStdGen)
 import UI.ReplayPlayer (runReplayApp)
-import qualified Wuss
+import Wuss (runSecureClient, runSecureClientWith)
+import qualified Data.List.NonEmpty as NE
+import Data.Text.Encoding (encodeUtf8)
+import Data.Binary (encode)
+import Data.Coerce (coerce)
+import Unsafe.Coerce (unsafeCoerce)
 
-newtype Client = Client {sConn :: WS.Connection}
+type MsgLenRep = Word8
 
+newtype TCPConn = TCPConn {getSocket :: Socket}
+
+lenBytes :: Int
+lenBytes = fromIntegral $ finiteBitSize @MsgLenRep 0 `div` 8
+
+-- | A way to mark which kind of bytestring message can be produced.
 type BSMessage a = ByteString
 
 type TextMessage a = Text.Text
@@ -48,42 +73,55 @@ gameEvToMessage (GameEvent tn ev) = looked <> tickNoToBytes tn
 
 type SeedMessage = BSMessage SeedSize
 
-sendSeedMessage :: WS.Connection -> SeedSize -> IO ()
-sendSeedMessage c = WS.sendBinaryData c . seedToMessage
+sendSeedMessage :: TCPConn -> SeedSize -> IO ()
+sendSeedMessage c = sendBSMessage c . seedToMessage
   where
     seedToMessage :: SeedSize -> SeedMessage
     seedToMessage = encode
 
 type ScoreMessage = BSMessage ScoreType
 
-sendScoreMessage :: WS.Connection -> ScoreType -> IO ()
-sendScoreMessage c = WS.sendBinaryData c . scoreToMessage
+sendScoreMessage :: TCPConn -> ScoreType -> IO ()
+sendScoreMessage c = sendBSMessage c . scoreToMessage
   where
     scoreToMessage :: ScoreType -> ScoreMessage
     scoreToMessage = encode
 
 type EventListMessage = BSMessage EventList
 
-sendEventList :: WS.Connection -> EventList -> IO ()
-sendEventList c = WS.sendBinaryData c . B.concat . map gameEvToMessage
+sendEventList :: TCPConn -> EventList -> IO ()
+sendEventList c = sendBSMessage c . B.concat . map gameEvToMessage
+
+sendBSMessage :: TCPConn -> ByteString -> IO ()
+sendBSMessage tcpConn msg =
+  sendAll (getSocket tcpConn) $
+    encode @MsgLenRep (fromIntegral $ B.length msg) <> msg
+
+sendTextMessage :: TCPConn -> Text.Text -> IO ()
+sendTextMessage tcpConn msg =
+  let txtMessage = B.fromStrict $ Text.encodeUtf8 msg
+  in sendAll (getSocket tcpConn) $
+      encode @MsgLenRep (fromIntegral $ Text.length msg) <> txtMessage
+
 -- sendEventList c = let
 --  evListSize = length evlist
 --  in do
-    -- WS.sendBinaryData c (B.singleton $ fromIntegral evListSize)
+-- WS.sendBinaryData c (B.singleton $ fromIntegral evListSize)
 
+closeConn :: TCPConn -> IO ()
+closeConn conn = gracefulClose (getSocket conn) 500
 
-closeConn :: WS.Connection -> IO ()
-closeConn conn = WS.sendClose conn ("Closing connection" :: ByteString)
+type NameMessage = BSMessage Name
 
-type NameMessage = TextMessage Name
-
-sendName :: WS.Connection -> Name -> IO ()
-sendName c = WS.sendTextData c . nameToMessage
+sendName :: TCPConn -> Name -> IO ()
+sendName c = sendTextMessage c . nameToMessage
   where
     nameToMessage = id
 
 runClientApp :: SeedSize -> ScoreType -> Text.Text -> [GameEvent] -> IO ()
-runClientApp seed score name evList = Wuss.runSecureClient "127.0.0.1" 34560 "/" app
+runClientApp seed score name evList =
+  -- withSocketsDo $ runTCPClient "127.0.0.1" 34561 app
+  withSocketsDo $ runTCPClient "haskell-server.tobioloke.com" 443 app
   where
     app c = do
       sendScoreMessage c score
@@ -91,16 +129,38 @@ runClientApp seed score name evList = Wuss.runSecureClient "127.0.0.1" 34560 "/"
       sendSeedMessage c seed
       sendEventList c evList
       closeConn c
+    
+    -- ackhdlr = TL.decodeUtf8
 
+runTCPClient :: HostName -> PortNumber -> (TCPConn -> IO b) -> IO b
+runTCPClient hostName port action = mask $ \restore -> do
+  addr <- resolve
+  tcpSock <- withAsync (connectClientTCPSocket addr) $ \sock -> wait sock
+  restore (action tcpSock) `finally` closeConn tcpSock
+  where
+    resolve = do
+      let hints = defaultHints {addrSocketType = Stream }
+      NE.head <$> getAddrInfo (Just hints) (Just hostName) (Just (show port))
+    
+    connectClientTCPSocket addr = do
+      sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+      setSocketOption sock NoDelay 1
+      connect sock $ addrAddress addr
+      pure $ TCPConn sock
 
 testClient :: IO ()
-testClient = let
-    moves = [1,3,8,10,1,11,14,1,1]
-    events = zipWith (GameEvent . TickNumber)
-              (scanl' (+) 1 moves)
-              [MoveRight, MoveDown, MoveLeft, MoveUp, MoveRight, MoveDown, MoveRight, MoveDown, MoveLeft]
-    in do
-        putStrLn $ "Sending seed: " ++  show (mkStdGen 4)
-        -- evs <- newMVar events
-        runClientApp 4 4 ("Max" :: Name) events
-        -- runReplayApp (mkStdGen 4) evs
+testClient =
+  let moves = [1, 3, 8, 10, 1, 11, 14, 1, 1]
+      events =
+        zipWith
+          (GameEvent . TickNumber)
+          (scanl' (+) 1 moves)
+          [MoveRight, MoveDown, MoveLeft, MoveUp, MoveRight, MoveDown, MoveRight, MoveDown, MoveLeft]
+   in do
+        putStrLn $ "Sending seed: " ++ show (mkStdGen 4)
+        -- 
+        runClientApp 4 4 ("MAX" :: Name) events
+
+-- repTest = do
+--  evs <- newMVar events
+--  runReplayApp (mkStdGen 4) evs
--- src/DB/Server.hs
+++ src/DB/Server.hs
@@ -1,50 +1,166 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module DB.Server where
 
 import Control.Concurrent (MVar, modifyMVar_, newMVar, readMVar, swapMVar)
 import Control.Exception (Exception, finally)
-import Control.Monad (void)
+import qualified Control.Exception as E
+import Control.Monad (forever, void, when)
+import Control.Monad.Trans (liftIO)
 import DB.Client
-import DB.Highscores (Score, Name, addScore, openDatabase)
+import DB.Highscores (Name, Score, addScore, openDatabase)
 import qualified Data.Bimap as BM
 import Data.Binary
 import qualified Data.ByteString.Lazy as B
 import qualified Data.IntMap.Strict as Map
-import GameLogic (GameState (getWorld, Playing), World (..), ScoreType, initWorld, defaultHeight, defaultWidth)
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import qualified Database.SQLite.Simple as DB
+import GameLogic (GameState (Playing, getWorld), ScoreType, World (..), defaultHeight, defaultWidth, initWorld)
 import Logging.Logger (EventList, GameEvent (..), TickNumber (..))
-import Logging.Replay (Seed, runReplayG, ReplayState (ReplayState))
+import Logging.Replay (ReplayState (ReplayState), Seed, runReplayG)
+import Network.Socket
+import Network.Socket.ByteString.Lazy
 import qualified Network.WebSockets as WS
 import System.Random (mkStdGen)
-import qualified Database.SQLite.Simple as DB
-import Data.Time.Clock.POSIX (getPOSIXTime)
-import Control.Monad.Trans (liftIO)
-import qualified Data.Text as T
 import UI.ReplayPlayer (runReplayApp)
 
 data ServerState = ServerState {clients :: ClientMap, currentIx :: CIndex}
 
-type ClientMap = Map.IntMap Client
+type ClientMap = Map.IntMap TCPConn
 
 type CIndex = Int
 
-data ServerStateError = MaxPlayers deriving (Show, Eq)
+type ClientConnection = TCPConn
+
+data ServerStateError = MaxPlayers | UnexpectedClose deriving stock (Show, Eq)
 
 instance Exception ServerStateError
 
 main :: IO ()
 main = do
-  putStrLn "Running server on localhost:34560 ..."
+  let port = (34561 :: PortNumber)
+  putStrLn $ "Running server on localhost:" <> show port <> " ..."
   state <- newMVar newServerState
   dbConn <- openDatabase "highscores.db"
-  WS.runServer "127.0.0.1" 34560 $ application state dbConn
+  -- Wuss.newSecureClientConnection "127.0.0.1" 34560 $ application state dbConn -- legacy
+  runTCPServer "127.0.0.1" port $ application state dbConn
+
+-- WS.runServer "127.0.0.1" port $ application state dbConn -- legacy
+--
+--
+--
+--
+-- Server stuff
+
+runTCPServer :: String -> PortNumber -> (Socket -> IO a) -> IO a
+runTCPServer host port app = forever $ withSocketsDo $ do
+  addr <- resolve
+  E.bracket (open addr) close app
+  where
+    resolve = do
+      let hints =
+            defaultHints
+              { addrFlags = [AI_PASSIVE],
+                addrSocketType = Stream
+              }
+      NE.head <$> getAddrInfo (Just hints) (Just host) (Just (show port))
+
+    open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+      setSocketOption sock ReuseAddr 1
+      withFdSocket sock setCloseOnExecIfNeeded
+      bind sock $ addrAddress addr
+      listen sock maxPlayers
+      pure sock
+
+application :: MVar ServerState -> DB.Connection -> Socket -> IO ()
+application state dbconn sock = do
+  (s, _) <- accept sock
+  appHandling state dbconn (TCPConn s)
+
+appHandling :: MVar ServerState -> DB.Connection -> ClientConnection -> IO ()
+appHandling state dbConn cliConn = do
+  st <- readMVar state
+  cix <- case addClient st cliConn of
+    Left MaxPlayers -> pure (-1) -- figure out what to do if the queue of scores being uploaded is full; ideally create a queue and process asynchronously while not full
+    Right newSt -> do
+      void $ swapMVar state newSt
+      pure (currentIx newSt)
+    Left _ -> error "Impossible"
+
+  flip finally (disconnect cix state) $
+    serverApp cix dbConn cliConn
+
+serverApp :: CIndex -> DB.Connection -> ClientConnection -> IO ()
+serverApp cix dbConn tcpConn = do
+  putStrLn $ "Client at index: " ++ show cix
+  s <- recvTCPData tcpConn messageToScore
+  putStrLn $ "Score of " ++ show s ++ " received"
+  name <- recvTCPData tcpConn messageToName
+  putStrLn $ "Name of " ++ show (T.toUpper name) ++ " received"
+  seed <- recvTCPData tcpConn messageToSeed
+  putStrLn $ "Seed: " ++ show seed
+  evList <- recvTCPData tcpConn handleEventList
+  -- putStrLn $ "First three events: " ++ show (take 3 evList)
+  putStrLn $ "All events: " ++ show evList
+  let initState =
+        ReplayState
+          (Playing $ initWorld defaultHeight defaultWidth seed)
+          (TickNumber 0)
+      !game = runReplayG evList initState
+      s' = (score . getWorld) game
+  if s /= s'
+    then do
+      putStrLn "Mismatched score!"
+      putStrLn $ "Expected score: " ++ show s
+      putStrLn $ "Actual score: " ++ show s'
+      print (getWorld game)
+    else -- evs <- newMVar evList
+    -- runReplayApp seed evs
+    -- error "Mismatch!"
+    do
+      putStrLn "Valid score" -- placeholder
+      time <- liftIO (round <$> getPOSIXTime)
+      -- addScore dbConn name s time
+      putStrLn $ "Score of " <> show s <> " by user " <> show cix <> " added"
+
+recvTCPData :: TCPConn -> (B.ByteString -> b) -> IO b
+recvTCPData tcpConn handler = do
+  lenPrefixBytes <- recvAll tcpConn (fromIntegral lenBytes)
+  let msglen = (fromIntegral . decode @MsgLenRep) lenPrefixBytes
+  handler <$> recvAll tcpConn msglen
+
+recvAll tcpConn size
+  | size <= 0 = pure $ B.empty
+  | otherwise = do
+      bs <- recv (getSocket tcpConn) size
+      -- putStrLn $ "Message size in bytes: " <> show size
+      -- print bs
+      when (B.null bs) $ E.throwIO UnexpectedClose
+      let len = B.length bs
+      if len < size
+        then do
+          let missing = size - len
+          rest <- recvAll tcpConn missing
+          pure $ bs <> rest
+        else pure bs
+
+-------
+--
+--
+--
+--
+--
+-- Constants and ServerState functions
 
 maxPlayers :: Int
-maxPlayers = 5
-
-defaultAcceptRequest :: WS.AcceptRequest
-defaultAcceptRequest = WS.defaultAcceptRequest
+maxPlayers = 6
 
 newServerState :: ServerState
 newServerState = ServerState mempty 0
@@ -52,26 +168,27 @@ newServerState = ServerState mempty 0
 numClients :: ServerState -> Int
 numClients = Map.size . clients
 
-addClient :: Client -> ServerState -> Either ServerStateError ServerState
-
 -- | Adds a client to the existing @ServerState@ at the current index. If the index is full, try the
--- (next one @mod@ 1024) recursively until a space is found. If there are more than maxPlayers, return an error.
-addClient c s@(ServerState cs ix)
+-- (next one @\`mod\`@ @maxPlayers@) recursively until a space is found. If there are more than maxPlayers, return an error.
+addClient :: ServerState -> TCPConn -> Either ServerStateError ServerState
+addClient s@(ServerState cs ix) c
   | numClients s >= maxPlayers = Left MaxPlayers
   | otherwise =
       maybe
         (Right $ ServerState (Map.insert ix c cs) (ix `mod` maxPlayers)) -- If successful return a new state and the index the current player was inserted at
-        (const $ addClient c $ s {currentIx = ix + 1 `mod` maxPlayers}) -- If a player still exists at our current index, try again at plus one
+        (const $ addClient s {currentIx = ix + 1 `mod` maxPlayers} c) -- If a player still exists at our current index, try again at plus one
         (Map.lookup ix (clients s)) -- Find the existing index
 
 removeClient :: CIndex -> ClientMap -> ClientMap
 removeClient = Map.delete
 
-application :: MVar ServerState -> DB.Connection -> WS.ServerApp
-application state dbconn pending = do
-  conn <- WS.acceptRequestWith pending defaultAcceptRequest
-  WS.withPingPong WS.defaultPingPongOptions conn $
-    appHandling state dbconn
+-- application :: MVar ServerState -> DB.Connection -> WS.ServerApp
+-- application state dbconn pending = do
+--   conn <- WS.acceptRequestWith pending defaultAcceptRequest
+--   WS.withPingPong WS.defaultPingPongOptions conn $
+--     appHandling state dbconn
+
+-- receiveBSMessage :: ByteString -> (BSMessage a -> a)
 
 disconnect :: CIndex -> MVar ServerState -> IO ()
 disconnect cix state = do
@@ -98,46 +215,40 @@ messageToScore :: ScoreMessage -> Score
 messageToScore = decode
 
 messageToName :: NameMessage -> Name
-messageToName = id
+messageToName = TL.toStrict . TL.decodeUtf8
 
-appHandling :: MVar ServerState -> DB.Connection -> WS.Connection -> IO ()
-appHandling state dbConn cliConn = do
-  st <- readMVar state
-  cix <- case addClient (Client cliConn) st of
-    Left MaxPlayers -> pure 0 -- figure out what to do if the queue of scores being uploaded is full; ideally create a queue and process asynchronously while not full
-    Right newSt -> do
-      void $ swapMVar state newSt
-      pure (currentIx newSt)
+-- serverApp cix (TCPConn sock) = do
+--   putStrLn $ "Client at index: " <> show cix
+--   messageToScore <$> tcpReceiveData sock
 
-  flip finally (disconnect cix state) $ do
-    putStrLn $ "Client at index: " ++ show cix
-    -- putStrLn ""
-    s <- messageToScore <$> WS.receiveData cliConn
-    putStrLn $ "Score of " ++ show s ++ " received"
-    name <- messageToName <$> WS.receiveData cliConn
-    putStrLn $ "Name of " ++ show (T.toUpper name) ++ " received"
-    seed <- messageToSeed <$> WS.receiveData cliConn
-    putStrLn $ "Seed: " ++ show seed
-    evList <- handleEventList <$> WS.receiveData cliConn
-    -- putStrLn $ "First three events: " ++ show (take 3 evList)
-    putStrLn $ "All events: " ++ show evList
-    let initState = ReplayState
-                      (Playing $ initWorld defaultHeight defaultWidth seed)
-                      (TickNumber 0)
-        !game = runReplayG evList initState
-        s' = (score. getWorld) game
-    if s /= s'
-      then do
-        putStrLn "Mismatched score!"
-        putStrLn $ "Expected score: " ++ show s
-        putStrLn $ "Actual score: " ++ show s'
-        print (getWorld game)
-        -- evs <- newMVar evList
-        -- runReplayApp seed evs
-        -- error "Mismatch!"
-      else do
-        putStrLn "Valid score" -- placeholder
-        time <- liftIO (round <$> getPOSIXTime)
-        -- addScore dbConn name s time
-        putStrLn $ "Score of " <> show s <> " by user " <> show cix <> " added"
-
+-- serverApp cix cliConn = do
+--   putStrLn $ "Client at index: " ++ show cix
+--   s <- messageToScore <$> WS.receiveData cliConn
+--   putStrLn $ "Score of " ++ show s ++ " received"
+--   name <- messageToName <$> WS.receiveData cliConn
+--   putStrLn $ "Name of " ++ show (T.toUpper name) ++ " received"
+--   seed <- messageToSeed <$> WS.receiveData cliConn
+--   putStrLn $ "Seed: " ++ show seed
+--   evList <- handleEventList <$> WS.receiveData cliConn
+--   -- putStrLn $ "First three events: " ++ show (take 3 evList)
+--   putStrLn $ "All events: " ++ show evList
+--   let initState =
+--         ReplayState
+--           (Playing $ initWorld defaultHeight defaultWidth seed)
+--           (TickNumber 0)
+--       !game = runReplayG evList initState
+--       s' = (score . getWorld) game
+--   if s /= s'
+--     then do
+--       putStrLn "Mismatched score!"
+--       putStrLn $ "Expected score: " ++ show s
+--       putStrLn $ "Actual score: " ++ show s'
+--       print (getWorld game)
+--     else -- evs <- newMVar evList
+--     -- runReplayApp seed evs
+--     -- error "Mismatch!"
+--     do
+--       putStrLn "Valid score" -- placeholder
+--       time <- liftIO (round <$> getPOSIXTime)
+--       -- addScore dbConn name s time
+--       putStrLn $ "Score of " <> show s <> " by user " <> show cix <> " added"
