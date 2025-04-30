b4d43260bb7d1eb771227f59a54dfa3bb6326044
Tobi Oloke <tobzdaman619@gmail.com>
2025-04-30 20:25:42 +0100

    Why is this a type error?

--- src/Logging/ServerLogger.hs
+++ src/Logging/ServerLogger.hs
@@ -5,49 +5,58 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE QuantifiedConstraints #-}
 
 module Logging.ServerLogger where
 
-import Bluefin.Compound (Handle (mapHandle), useImplIn)
 import Bluefin.Eff
 import Bluefin.IO
-import Bluefin.Reader (Reader, ask, runReader)
-import Bluefin.State (State, evalState, get, modify)
-import Bluefin.Stream (Stream, yield)
-import Brick (BrickEvent (..))
-import qualified Brick.Keybindings as K
-import Control.Concurrent.STM (TChan, isEmptyTChan, readTChan, tryReadTChan)
-import Control.Monad (unless)
-import Control.Monad.IO.Class (MonadIO (..))
+import Bluefin.Stream (Stream, yield, consumeStream, yieldToList)
+import Control.Concurrent.STM (TChan, tryReadTChan, newTChanIO, writeTChan, newTChan)
 import Control.Monad.STM (atomically)
 import Data.Text (Text)
 import qualified Data.Text as Text
-import Data.Word (Word16)
-import Graphics.Vty.CrossPlatform as V ()
-import qualified Graphics.Vty.Input as V
 import Network.Socket (SockAddr)
+import Bluefin.Writer (Writer, tell, execWriter)
+import Bluefin.Compound (useImplIn, mapHandle)
 
-data Logger i o e = Logger i (Stream o e)
+data Logger i o e = Logger (Stream o e)
 
-data ClientLog = ClientLog {clientID :: Int, clientAddr :: SockAddr, clientMsg :: Text}
+data ClientLogItem = ClientLog {clientID :: Int, clientAddr :: SockAddr, clientMsg :: Text}
 
-instance Show ClientLog where
+instance Show ClientLogItem where
   show (ClientLog iD sockAddr msg) =
-    unlines
-      [ replicate 80 '=',
-        "Client ID: " <> show iD,
-        "Client Addr: " <> show sockAddr,
-        if not (Text.null msg) then "Message: " <> Text.unpack msg else ""
-      ]
+    "Client ID: " <> show iD <> "| Client Addr: " <> show sockAddr <> ": " <> Text.unpack msg
 
-type ClientLogger = Logger (TChan ClientLog) String
+type ClientLogger = Logger (TChan ClientLogItem) Text
 
-logClient :: (e :> es) => IOE e -> ClientLogger e -> Eff es ()
-logClient io l@(Logger clq str) = do
+sendToLog :: (e :> es) => IOE e -> TChan ClientLogItem -> ClientLogItem  -> ClientLogger e -> Eff es ()
+sendToLog io clq cl (Logger str) = do
+  yield str . Text.pack $ "Writing clientID " <> show (clientID cl) <> " to the pack"
+  effIO io . atomically . writeTChan clq $ cl
+  
+
+logClient :: (e :> es, Show i) => IOE e -> TChan i -> Logger i Text e -> Eff es ()
+logClient io clq l@(Logger str) = do
   res <- effIO io . atomically $ tryReadTChan clq
   case res of
     Nothing -> pure ()
-    Just cl -> do 
-      yield str (show cl)
-      logClient io l
-    
+    Just cl -> do
+      yield str (Text.pack . show $ cl)
+      logClient io clq l
+
+runClientLogger :: (forall e. ClientLogger e -> Eff (e :& es) r) -> Eff es [Text]
+runClientLogger k = fst <$> yieldToList (\y -> useImplIn k (Logger (mapHandle y)))
+
+-- newLogger :: (forall e. ClientLogger e -> Eff (e :& es) r) -> IO [Text]
+-- testLogger :: IO [Text]
+testLogger = runEff $ \io -> do
+  chan <- effIO io newTChanIO
+  runClientLogger $ \l -> do
+    logClient io chan l
+    -- sendToLog io chan (ClientLog undefined undefined undefined) l
+
+
+  
+
+
