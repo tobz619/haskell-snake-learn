5dfecb45f19d8b628a0abf51da02f3ad02e566ec
Tobi Oloke <tobzdaman619@gmail.com>
2025-06-04 07:44:32 +0100

    WORKING REWINDS IN REPLAYS!

--- src/Logging/Replay.hs
+++ src/Logging/Replay.hs
@@ -20,8 +20,8 @@ data ReplayState = ReplayState
   { rGameState :: !GameState,
     rTickNo :: !TickNumber,
     rCheckPoint :: CheckPointMap,
-    rGameStateVec :: Either RewindBuffer [(Int, GameState)],
-    rEvIndex :: !Int,
+    rGameStateVec :: Either RewindBuffer [RewindType],
+    rEvIndex :: !EvNumber,
     rRewindIndex :: !Int
   }
 
@@ -45,7 +45,7 @@ runReplay evs = do
   runMove evs
 
 canExecute :: InputList -> ReplayState -> Maybe GameEvent
-canExecute evList (ReplayState _ t1 _ _ ix _) =
+canExecute evList (ReplayState _ t1 _ _ (EvNumber ix) _) =
   evList V.!? ix >>= \g@(GameEvent t0 _) -> if t0 == t1
                       then pure g
                       else Nothing
@@ -69,11 +69,14 @@ addCheckPoint evList = do
 handleSpeed :: MonadState ReplayState m => Float -> InputList -> m ()
 handleSpeed s evList
   | s > 0 = do
-             rps <- get
-             let rws = rGameStateVec rps
-             either (stepRewindF evList) (\_ -> addCheckPoint evList >> runMove evList) rws
+             rws <- gets rGameStateVec
+             either (stepRewindF evList) (\_ -> runMove evList >> addCheckPoint evList) rws
+             gs <- gets rGameState
+             unless (isGameOver gs) $ modify stepReplayState
 
   | otherwise = do
+            gs <- gets rGameState
+            unless (isGameOver gs) $ modify stepReplayState
             rps <- get
             let rws = either id mkRewindBuffer (rGameStateVec rps)
             modify (\r -> r {rGameStateVec = Left rws})
@@ -85,70 +88,72 @@ runMove evList = do
       rps <- get
       mapM_
         (\(GameEvent _ kev) -> do
-          modify $ \r -> r {rGameState = executeMove kev (rGameState rps) pairs}
-          modify $ \r -> r {rEvIndex = rEvIndex rps + 1}
-          -- runMove evList -- Make sure all events are executed at that moment!
+          modify $ \r -> r {rGameState = executeMove kev (rGameState r) pairs}
+          modify $ \r -> r {rEvIndex = rEvIndex r + 1}
         )
         (canExecute evList rps)
-      gs <- gets rGameState
-      unless (isGameOver gs) $ modify stepReplayState
 
 
-stepRewindF :: MonadState ReplayState m => InputList -> RewindBuffer -> m ()
+-- stepRewindF :: MonadState ReplayState m =>  RewindBuffer -> m ()
 stepRewindF evList arr = do
   ix <- gets rRewindIndex
-  if ix < 0
+  if ix <= 0
     then do
     modify (\rps -> rps {
-      rGameStateVec = pure (V.toList arr),
-      rEvIndex = fst $ arr V.! 0,
-      rRewindIndex = 0
+      rGameStateVec = pure . V.toList $ arr,
+      rEvIndex = rwEv $ arr V.! ix,
+      rTickNo = rwTick $ arr V.! ix
       }
       )
     runMove evList
   else do
-      modify (\rps -> rps {
-        rGameState = snd $ arr V.! rRewindIndex rps,
-        rEvIndex =  fst $ arr V.! rRewindIndex rps,
-        rTickNo = rTickNo rps + 1
-        }
-        )
-      modify decreaseRewindIndex
+    modify (\rps -> rps {
+      rGameState = rwGS $ arr V.! ix,
+      rEvIndex =  rwEv $ arr V.! ix,
+      rTickNo = rwTick $ arr V.! ix
+      }
+      )
+    modify decreaseRewindIndex
 
 
 
 stepRewindR :: MonadState ReplayState m => RewindBuffer -> m ()
 stepRewindR arr = do
   ix <- gets rRewindIndex
-  unless (ix >= endOfArray) $ do
-      modify (increaseRewindIndex arr)
-      modify (\rps -> rps {
-        rGameState = snd $ arr V.! rRewindIndex rps,
-        rEvIndex = fst $ arr V.! rRewindIndex rps,
-        rTickNo = rTickNo rps - 1
+  modify $ \rps -> rps {
+        rGameState = rwGS $ arr V.! ix,
+        rEvIndex = rwEv $ arr V.! ix,
+        rTickNo = rwTick $ arr V.! ix
         }
-        )
+  unless (ix >= endOfArray) $ do
+     modify $ \rps -> rps {
+        rTickNo = rwTick $ arr V.! ix
+        }
+
+  modify (increaseRewindIndex arr)
+
     where endOfArray = V.length arr - 1
 
 increaseRewindIndex :: RewindBuffer -> ReplayState -> ReplayState
 increaseRewindIndex arr rps@(ReplayState {..})
-  | rRewindIndex >= V.length arr = rps {rRewindIndex = V.length arr}
-  | otherwise = rps {rRewindIndex = rRewindIndex + 1}
+  | rRewindIndex >= V.length arr - 1 = rps {rRewindIndex = V.length arr - 1 }
+  | otherwise = rps { rRewindIndex = rRewindIndex + 1 }
 
 decreaseRewindIndex :: ReplayState -> ReplayState
 decreaseRewindIndex rps@(ReplayState{..})
-  | rRewindIndex < 0 = rps
-  | otherwise = rps {rRewindIndex = rRewindIndex - 1}
+  | rRewindIndex < 0 = rps {rRewindIndex = 0}
+  | otherwise = rps { rRewindIndex = rRewindIndex - 1 }
 
 stepReplayState :: ReplayState -> ReplayState
 stepReplayState (ReplayState {..}) =
-  ReplayState
-    (stepGameState rGameState)
-    (rTickNo + 1)
-    rCheckPoint
-    (((rEvIndex, rGameState) :) <$> rGameStateVec)
-    rEvIndex
-    rRewindIndex
+  ReplayState {
+    rGameState = stepGameState rGameState,
+    rTickNo = rTickNo + 1,
+    rCheckPoint = rCheckPoint,
+    rGameStateVec = (RewindType rTickNo rEvIndex rGameState :) <$> rGameStateVec, 
+    rEvIndex = rEvIndex, 
+    rRewindIndex = rRewindIndex 
+    }
 
 
 executeMove :: KeyEvent -> GameState -> Map.Map KeyEvent (GameState -> GameState) -> GameState
--- src/UI/ReplayPlayer.hs
+++ src/UI/ReplayPlayer.hs
@@ -1,16 +1,20 @@
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module UI.ReplayPlayer where
 
-import Brick (App (..), BrickEvent (AppEvent, VtyEvent), EventM, customMain, neverShowCursor)
+import Brick (App (..), BrickEvent (AppEvent, VtyEvent), EventM, customMain, gets, neverShowCursor)
 import Brick.BChan (newBChan, writeBChan)
 import Brick.Main (halt)
 import Brick.Types (Widget)
 import Brick.Widgets.Center (center)
-import Control.Concurrent (MVar, forkIO, modifyMVar_, readMVar, threadDelay, newMVar)
+import Brick.Widgets.Core
+import Control.Concurrent (MVar, forkIO, modifyMVar_, newMVar, readMVar, threadDelay)
 import Control.Monad (forever, void)
 import Control.Monad.IO.Class (liftIO)
 import qualified Data.Map.Strict as Map
+import qualified Data.Text as Text
+import qualified Data.Vector.Strict as Vec
 import GameLogic (GameState (GameOver, NewHighScore, Playing), defaultHeight, defaultWidth, initWorld)
 import qualified Graphics.Vty as V
 import qualified Graphics.Vty.CrossPlatform as V
@@ -19,9 +23,6 @@ import Logging.Replay (ReplayState (..), handleSpeed)
 import System.Random (StdGen, mkStdGen)
 import UI.Gameplay (drawGS, theMap)
 import UI.Types
-import Brick (gets)
-import Brick.Widgets.Core
-import qualified Data.Text as Text
 
 runReplayApp :: StdGen -> InputList -> MVar Float -> IO ()
 runReplayApp seed mEvList mSpeedMod = do
@@ -37,7 +38,7 @@ runReplayApp seed mEvList mSpeedMod = do
 replayApp :: InputList -> MVar Float -> App ReplayState Tick MenuOptions
 replayApp evs speedMod =
   App
-    { appDraw = drawUI,
+    { appDraw = flip drawUI evs,
       appChooseCursor = neverShowCursor,
       appHandleEvent = \brickEv -> replayEventHandler brickEv evs speedMod,
       appStartEvent = replayEventHandler (AppEvent Tick) evs speedMod,
@@ -46,10 +47,10 @@ replayApp evs speedMod =
 
 replayEventHandler :: BrickEvent n Tick -> InputList -> MVar Float -> EventM n ReplayState ()
 replayEventHandler (VtyEvent (V.EvKey (V.KChar 'c') [V.MCtrl])) _ _ = halt
-replayEventHandler (VtyEvent (V.EvKey k _)) _ mSpeedMod = gets rEvIndex >>= \ix -> liftIO $ do
+replayEventHandler (VtyEvent (V.EvKey k _)) _ mSpeedMod = liftIO $ do
   case k of
     V.KChar 'n' -> modifyMVar_ mSpeedMod (pure . normalSpeed)
-    V.KChar 'r' -> modifyMVar_ mSpeedMod (pure . negate) >>  print ix
+    V.KChar 'r' -> modifyMVar_ mSpeedMod (pure . negate)
     V.KLeft -> modifyMVar_ mSpeedMod (pure . speedDown)
     V.KRight -> modifyMVar_ mSpeedMod (pure . speedUp)
     _ -> pure ()
@@ -59,36 +60,44 @@ replayEventHandler (AppEvent Tick) evList mSpeedMod = do
 replayEventHandler _ _ _ = pure ()
 
 initState :: StdGen -> ReplayState
-initState seed = ReplayState {
-    rTickNo = TickNumber 0
-  , rGameState = start
-  , rGameStateVec = Right []
-  , rCheckPoint = Map.empty
-  , rEvIndex = 0
-  , rRewindIndex = 0
-  }
+initState seed =
+  ReplayState
+    { rTickNo = TickNumber 0,
+      rGameState = start,
+      rGameStateVec = Right [RewindType (TickNumber 0) (EvNumber 0) start],
+      rCheckPoint = Map.empty,
+      rEvIndex = 0,
+      rRewindIndex = 0
+    }
   where
     start = Playing $ initWorld defaultHeight defaultWidth seed
 
-drawUI :: ReplayState -> [Widget MenuOptions]
-drawUI rps = [drawDebug rps] <> (center <$> drawGS gs)
+drawUI :: ReplayState -> InputList -> [Widget MenuOptions]
+drawUI rps evList = [drawDebug rps evList] <> (center <$> drawGS gs)
   where
     gs = case rGameState rps of
       NewHighScore w -> GameOver w -- Short circuit the GameState to GameOver so it renders properly
       g -> g
 
-
-drawDebug :: ReplayState -> Widget n
-drawDebug gps = currentTick
+drawDebug :: ReplayState -> InputList -> Widget n
+drawDebug gps evList = currentTick <=> nextEvTick <=> evIndex <=> rIndex <=> arrLen <=> evLen
   where
-    currentTick = hLimit 10 $ vBox [txt $ Text.pack $ show $ (rTickNo gps)]
-    -- currentLog = vLimit 20 $ hLimit 45 $ vBox $ txt . Text.pack . show <$> take 20 (rEventList gps)
+    eIndex = (\(EvNumber x) -> x) (rEvIndex gps)
+    evNumber (GameEvent t _) = t
+    currentTick = hLimit 15 $ hBox [txt "Tick: ", txt $ Text.pack $ show $ rTickNo gps]
+    evIndex = hLimit 15 $ hBox [txt "Ev Index: ", txt $ Text.pack $ show $ rEvIndex gps]
+    rIndex = hLimit 15 $ hBox [txt "Rew Index: ", txt $ Text.pack $ show $ rRewindIndex gps]
+    nextEvTick = hLimit 15 $ vBox [txt "Nx Ev Tick: ", txt $ Text.pack $ show $ evNumber <$> evList Vec.!? eIndex]
+    arrLen = hLimit 15 $ hBox [txt "Arr Len: ", txt $ Text.pack $ show $ either Vec.length length (rGameStateVec gps)]
+    evLen = hLimit 15 $ hBox [txt "Ev Len: ", txt $ Text.pack $ show $ Vec.length evList]
+
+-- currentLog = vLimit 20 $ hLimit 45 $ vBox $ txt . Text.pack . show <$> take 20 (rEventList gps)
 
 replayExample :: IO ()
 replayExample = do
-  let evs = mkInputList evs2
+  let evs = mkInputList evs3
   speed <- newMVar 1
-  runReplayApp (mkStdGen seed2) evs speed
+  runReplayApp (mkStdGen seed3) evs speed
 
 -- where
 --   moves = [1, 3, 8, 10, 1, 11, 14, 1, 1]
@@ -103,14 +112,17 @@ speedUp x
   | x >= 16 = x
   | otherwise = x * 2
 speedDown x
-  | x <= (1 / 4) = x
+  | abs x <= (1 / 8) = x
   | otherwise = x / 2
 normalSpeed = const 1
 
-mkEvs = reverse . filter (\(GameEvent _ x) -> x `elem` [MoveUp, MoveDown, MoveLeft, MoveRight])
+mkEvs = reverse  . filter (\(GameEvent _ x) -> isMovement x)
+  where
+    isMovement x = x `elem` [MoveUp, MoveDown, MoveLeft, MoveRight]
 
 evs2 :: [GameEvent]
-evs2 = mkEvs
+evs2 =
+  mkEvs
     [GameEvent 534 GameEnded, GameEvent 533 (FoodEaten (V2 10 19)), GameEvent 525 MoveUp, GameEvent 519 MoveLeft, GameEvent 513 MoveDown, GameEvent 511 (FoodEaten (V2 14 17)), GameEvent 500 MoveRight, GameEvent 499 MoveUp, GameEvent 484 (FoodEaten (V2 18 16)), GameEvent 483 MoveLeft, GameEvent 476 MoveUp, GameEvent 470 MoveRight, GameEvent 462 MoveUp, GameEvent 456 (FoodEaten (V2 19 1)), GameEvent 456 MoveLeft, GameEvent 450 MoveDown, GameEvent 435 MoveRight, GameEvent 432 MoveUp, GameEvent 424 MoveLeft, GameEvent 423 (FoodEaten (V2 12 5)), GameEvent 415 MoveDown, GameEvent 410 MoveRight, GameEvent 409 (FoodEaten (V2 7 12)), GameEvent 400 MoveUp, GameEvent 392 MoveLeft, GameEvent 384 MoveDown, GameEvent 382 (FoodEaten (V2 13 11)), GameEvent 381 MoveRight, GameEvent 374 MoveUp, GameEvent 364 MoveRight, GameEvent 360 (FoodEaten (V2 2 8)), GameEvent 360 MoveDown, GameEvent 356 MoveLeft, GameEvent 349 MoveDown, GameEvent 346 MoveRight, GameEvent 338 MoveUp, GameEvent 328 MoveLeft, GameEvent 321 MoveDown, GameEvent 315 MoveRight, GameEvent 313 (FoodEaten (V2 7 12)), GameEvent 312 MoveUp, GameEvent 306 MoveLeft, GameEvent 297 MoveUp, GameEvent 291 MoveRight, GameEvent 290 (FoodEaten (V2 7 3)), GameEvent 287 MoveDown, GameEvent 278 MoveLeft, GameEvent 273 MoveDown, GameEvent 271 (FoodEaten (V2 14 11)), GameEvent 267 MoveRight, GameEvent 262 MoveUp, GameEvent 257 MoveLeft, GameEvent 256 (FoodEaten (V2 15 7)), GameEvent 251 MoveDown, GameEvent 248 MoveRight, GameEvent 241 (FoodEaten (V2 12 5)), GameEvent 241 MoveUp, GameEvent 235 MoveRight, GameEvent 231 MoveDown, GameEvent 229 (FoodEaten (V2 8 9)), GameEvent 226 MoveLeft, GameEvent 222 MoveDown, GameEvent 218 MoveRight, GameEvent 212 MoveUp, GameEvent 208 MoveLeft, GameEvent 207 (FoodEaten (V2 11 8)), GameEvent 201 MoveDown, GameEvent 197 MoveRight, GameEvent 192 MoveDown, GameEvent 190 (FoodEaten (V2 9 19)), GameEvent 184 MoveLeft, GameEvent 169 MoveUp, GameEvent 164 (FoodEaten (V2 10 4)), GameEvent 163 MoveRight, GameEvent 156 MoveDown, GameEvent 149 (FoodEaten (V2 16 11)), GameEvent 148 MoveLeft, GameEvent 142 MoveDown, GameEvent 140 MoveRight, GameEvent 132 MoveUp, GameEvent 122 MoveRight, GameEvent 118 MoveDown, GameEvent 116 (FoodEaten (V2 7 13)), GameEvent 115 MoveLeft, GameEvent 105 MoveUp, GameEvent 94 MoveLeft, GameEvent 93 (FoodEaten (V2 19 4)), GameEvent 85 MoveDown, GameEvent 81 MoveRight, GameEvent 76 MoveDown, GameEvent 73 (FoodEaten (V2 12 17)), GameEvent 72 MoveRight, GameEvent 66 MoveUp, GameEvent 59 (FoodEaten (V2 18 11)), GameEvent 58 MoveLeft, GameEvent 48 MoveUp, GameEvent 31 (FoodEaten (V2 2 1)), GameEvent 30 MoveRight, GameEvent 26 MoveDown, GameEvent 12 (FoodEaten (V2 15 5)), GameEvent 11 MoveLeft, GameEvent 6 MoveDown, GameEvent 0 MoveRight]
 
 seed2 :: SeedType
@@ -119,5 +131,6 @@ seed2 = 9157570410566699885
 seed3 = 8708303021790635255
 
 evs3 :: [GameEvent]
-evs3 = mkEvs
-  [GameEvent 574 GameEnded,GameEvent 568 MoveRight,GameEvent 563 MoveUp,GameEvent 559 (FoodEaten (V2 10 2)),GameEvent 557 MoveLeft,GameEvent 540 MoveDown,GameEvent 531 MoveRight,GameEvent 528 MoveUp,GameEvent 527 (FoodEaten (V2 4 16)),GameEvent 522 MoveLeft,GameEvent 517 MoveUp,GameEvent 510 MoveRight,GameEvent 508 (FoodEaten (V2 1 8)),GameEvent 504 MoveUp,GameEvent 490 MoveLeft,GameEvent 486 MoveUp,GameEvent 475 MoveRight,GameEvent 475 (FoodEaten (V2 4 2)),GameEvent 473 MoveDown,GameEvent 467 MoveLeft,GameEvent 459 MoveDown,GameEvent 452 MoveRight,GameEvent 448 (FoodEaten (V2 3 6)),GameEvent 444 MoveUp,GameEvent 436 MoveLeft,GameEvent 434 MoveDown,GameEvent 427 MoveRight,GameEvent 426 (FoodEaten (V2 4 7)),GameEvent 423 MoveDown,GameEvent 408 MoveLeft,GameEvent 404 (FoodEaten (V2 19 14)),GameEvent 400 MoveDown,GameEvent 396 MoveRight,GameEvent 382 MoveUp,GameEvent 380 (FoodEaten (V2 12 3)),GameEvent 374 MoveRight,GameEvent 370 MoveDown,GameEvent 366 MoveLeft,GameEvent 364 (FoodEaten (V2 11 10)),GameEvent 359 MoveDown,GameEvent 349 MoveRight,GameEvent 345 (FoodEaten (V2 1 9)),GameEvent 339 MoveUp,GameEvent 329 MoveLeft,GameEvent 326 MoveUp,GameEvent 320 (FoodEaten (V2 4 1)),GameEvent 319 MoveRight,GameEvent 315 MoveDown,GameEvent 302 MoveLeft,GameEvent 301 MoveUp,GameEvent 291 (FoodEaten (V2 6 4)),GameEvent 289 MoveRight,GameEvent 286 MoveUp,GameEvent 278 (FoodEaten (V2 14 1)),GameEvent 277 MoveLeft,GameEvent 267 MoveDown,GameEvent 258 MoveRight,GameEvent 254 (FoodEaten (V2 5 6)),GameEvent 249 MoveUp,GameEvent 237 MoveLeft,GameEvent 237 (FoodEaten (V2 17 3)),GameEvent 229 MoveDown,GameEvent 215 MoveRight,GameEvent 211 MoveDown,GameEvent 208 (FoodEaten (V2 7 14)),GameEvent 206 MoveLeft,GameEvent 195 MoveUp,GameEvent 186 (FoodEaten (V2 18 3)),GameEvent 184 MoveLeft,GameEvent 183 MoveUp,GameEvent 169 MoveRight,GameEvent 169 (FoodEaten (V2 5 3)),GameEvent 165 MoveDown,GameEvent 163 MoveLeft,GameEvent 158 MoveDown,GameEvent 155 MoveRight,GameEvent 151 MoveUp,GameEvent 149 (FoodEaten (V2 7 7)),GameEvent 146 MoveLeft,GameEvent 137 MoveDown,GameEvent 131 (FoodEaten (V2 2 16)),GameEvent 129 MoveRight,GameEvent 124 MoveUp,GameEvent 113 (FoodEaten (V2 13 11)),GameEvent 108 MoveLeft,GameEvent 102 MoveDown,GameEvent 98 (FoodEaten (V2 12 17)),GameEvent 89 MoveRight,GameEvent 75 MoveUp,GameEvent 74 (FoodEaten (V2 6 3)),GameEvent 72 MoveLeft,GameEvent 59 MoveDown,GameEvent 48 MoveLeft,GameEvent 45 MoveDown,GameEvent 45 (FoodEaten (V2 17 19)),GameEvent 38 MoveRight,GameEvent 35 MoveUp,GameEvent 31 MoveRight,GameEvent 29 MoveDown,GameEvent 17 MoveLeft,GameEvent 15 (FoodEaten (V2 19 15)),GameEvent 9 MoveUp,GameEvent 0 MoveRight,GameEvent 0 GameStarted]
+evs3 =
+  mkEvs
+    [GameEvent 574 GameEnded, GameEvent 568 MoveRight, GameEvent 563 MoveUp, GameEvent 559 (FoodEaten (V2 10 2)), GameEvent 557 MoveLeft, GameEvent 540 MoveDown, GameEvent 531 MoveRight, GameEvent 528 MoveUp, GameEvent 527 (FoodEaten (V2 4 16)), GameEvent 522 MoveLeft, GameEvent 517 MoveUp, GameEvent 510 MoveRight, GameEvent 508 (FoodEaten (V2 1 8)), GameEvent 504 MoveUp, GameEvent 490 MoveLeft, GameEvent 486 MoveUp, GameEvent 475 MoveRight, GameEvent 475 (FoodEaten (V2 4 2)), GameEvent 473 MoveDown, GameEvent 467 MoveLeft, GameEvent 459 MoveDown, GameEvent 452 MoveRight, GameEvent 448 (FoodEaten (V2 3 6)), GameEvent 444 MoveUp, GameEvent 436 MoveLeft, GameEvent 434 MoveDown, GameEvent 427 MoveRight, GameEvent 426 (FoodEaten (V2 4 7)), GameEvent 423 MoveDown, GameEvent 408 MoveLeft, GameEvent 404 (FoodEaten (V2 19 14)), GameEvent 400 MoveDown, GameEvent 396 MoveRight, GameEvent 382 MoveUp, GameEvent 380 (FoodEaten (V2 12 3)), GameEvent 374 MoveRight, GameEvent 370 MoveDown, GameEvent 366 MoveLeft, GameEvent 364 (FoodEaten (V2 11 10)), GameEvent 359 MoveDown, GameEvent 349 MoveRight, GameEvent 345 (FoodEaten (V2 1 9)), GameEvent 339 MoveUp, GameEvent 329 MoveLeft, GameEvent 326 MoveUp, GameEvent 320 (FoodEaten (V2 4 1)), GameEvent 319 MoveRight, GameEvent 315 MoveDown, GameEvent 302 MoveLeft, GameEvent 301 MoveUp, GameEvent 291 (FoodEaten (V2 6 4)), GameEvent 289 MoveRight, GameEvent 286 MoveUp, GameEvent 278 (FoodEaten (V2 14 1)), GameEvent 277 MoveLeft, GameEvent 267 MoveDown, GameEvent 258 MoveRight, GameEvent 254 (FoodEaten (V2 5 6)), GameEvent 249 MoveUp, GameEvent 237 MoveLeft, GameEvent 237 (FoodEaten (V2 17 3)), GameEvent 229 MoveDown, GameEvent 215 MoveRight, GameEvent 211 MoveDown, GameEvent 208 (FoodEaten (V2 7 14)), GameEvent 206 MoveLeft, GameEvent 195 MoveUp, GameEvent 186 (FoodEaten (V2 18 3)), GameEvent 184 MoveLeft, GameEvent 183 MoveUp, GameEvent 169 MoveRight, GameEvent 169 (FoodEaten (V2 5 3)), GameEvent 165 MoveDown, GameEvent 163 MoveLeft, GameEvent 158 MoveDown, GameEvent 155 MoveRight, GameEvent 151 MoveUp, GameEvent 149 (FoodEaten (V2 7 7)), GameEvent 146 MoveLeft, GameEvent 137 MoveDown, GameEvent 131 (FoodEaten (V2 2 16)), GameEvent 129 MoveRight, GameEvent 124 MoveUp, GameEvent 113 (FoodEaten (V2 13 11)), GameEvent 108 MoveLeft, GameEvent 102 MoveDown, GameEvent 98 (FoodEaten (V2 12 17)), GameEvent 89 MoveRight, GameEvent 75 MoveUp, GameEvent 74 (FoodEaten (V2 6 3)), GameEvent 72 MoveLeft, GameEvent 59 MoveDown, GameEvent 48 MoveLeft, GameEvent 45 MoveDown, GameEvent 45 (FoodEaten (V2 17 19)), GameEvent 38 MoveRight, GameEvent 35 MoveUp, GameEvent 31 MoveRight, GameEvent 29 MoveDown, GameEvent 17 MoveLeft, GameEvent 15 (FoodEaten (V2 19 15)), GameEvent 9 MoveUp, GameEvent 0 MoveRight, GameEvent 0 GameStarted]
