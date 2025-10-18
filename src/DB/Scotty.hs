37a5527d09fc24bc84eeda2f15f28adc6e6d0f01
Tobi Oloke <tobzdaman619@gmail.com>
2025-10-18 20:35:52 +0100

    RESTful send is working without problems :)

--- src/DB/Scotty.hs
+++ src/DB/Scotty.hs
@@ -20,15 +20,17 @@ import DB.Receive (handleEventList, textWriteTChan)
 import System.Random (mkStdGen)
 import Control.Concurrent.STM (TChan)
 import Data.Text (Text)
+import Data.List (find)
 
 scottyAPI :: TChan Text -> DB.Connection -> ScottyM ()
 scottyAPI msgChan conn = do
-  get "/leaderBoardQuery/:pageIndex/:pageSize" $ getScoreSlice' msgChan conn
+  get "/leaderboardQuery/:pageIndex/:pageSize" $ getScoreSlice' msgChan conn
   post "/addScore/:name/:score/:seedValue" $ addScoreToDB' msgChan conn
 
 getScoreSlice' :: TChan Text -> DB.Connection -> ActionM ()
 getScoreSlice' msgChan dbConn = do
-  liftIO $ textWriteTChan msgChan "Getting score slice"
+  ip <- getIPFromHeaders
+  liftIO $ textWriteTChan msgChan $ "Remote IP: " ++ ip
   pIx <- PageNumber <$> pathParam "pageIndex"
   pSize <- PageHeight <$> pathParam "pageSize"
   ps <- liftIO $ getScoreSlice pIx pSize dbConn
@@ -36,17 +38,24 @@ getScoreSlice' msgChan dbConn = do
     [] -> do
       status status400
       text "No values returned"
-    ps -> do
+    ps' -> do
       status status200
-      text $ T.pack . show $ ps
+      text $ T.pack . show $ ps'
+
+  where getIPFromHeaders = do
+            hds <- headers
+            let res = maybe "N/A" snd $ find ((== "HTTP_CLIENT_IP") . fst ) hds
+            pure (T.unpack res) 
 
 addScoreToDB' :: TChan Text -> DB.Connection -> ActionM ()
 addScoreToDB' msgChan dbConn = do
   name <- pathParam  @NameType "name"
   s <- pathParam @ScoreType "score"
   seed <- pathParam @SeedType "seedValue"
-  evListBytes <- B8.take 8192 <$> body
+  evListStr <- B8.unpack . B8.take 8192 <$> body
+  liftIO $ putStrLn evListStr
   let evList = mkEvs $ handleEventList evListBytes
+      evListBytes = read @EventListMessage evListStr
       !game = runReplayG evList (initState (mkStdGen seed))
       s' = (score . getWorld) game
   
@@ -62,7 +71,7 @@ addScoreToDB' msgChan dbConn = do
       liftIO $ textWriteTChan msgChan $ "Valid score"
       time <- liftIO (round <$> getPOSIXTime)
       liftIO $ addScoreWithReplay name s time seed evListBytes dbConn
-      liftIO $ textWriteTChan msgChan $ "Score of " <> show s <> "added"
+      liftIO $ textWriteTChan msgChan $ "Score of " <> show s <> " added"
       status status201
       text "OK"
 
